c028f09227f18572d88f600b2bd1b30e2448ac97
javazquezm <javazquezm@tracsa.com.mx>
2026-09-07 12:14:43 -0600

    chore: eliminar módulo principal y demás residuos del template original sin usar

--- src/modules/principal/services/dashboardService.js
+++
@@ -1,6 +0,0 @@
-import mainApi from '@/api/mainApi';
-
-export const obtenerSistemas = async (showGlobalLoading = true) => {
-  const { data } = await mainApi.get('/api/auth/sistemas', { showGlobalLoading });
-  return data.data;
-};
